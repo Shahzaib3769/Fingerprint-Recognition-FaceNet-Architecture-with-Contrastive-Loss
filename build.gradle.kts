1648cd356f06f97f5c30dd306583b5f882f94a3e
Shahzaib Ahmed <117628818+Shahzaib3769@users.noreply.github.com>
2025-06-17 04:57:04 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,5 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-}
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "1.5.1" apply false
 }
